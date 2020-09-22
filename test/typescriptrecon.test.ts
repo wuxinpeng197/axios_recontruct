f19ff68e8ac7ac740cabd5696fad0447541a7f6c
wuxinpeng197 <xinpengwu193@gmail.com>
2020-09-22 15:32:09 +1000

    firsr_commit

---
+++ test/typescriptrecon.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/typescriptrecon"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
